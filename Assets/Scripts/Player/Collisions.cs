1d68be718ec8b40003897797cf6c75d2ede4b6f4
GitIn-space <martin@cjo.se>
2021-10-05 15:29:07 +0200

    Added spikes, collisions are consolidated into one script

---
+++ Assets/Scripts/Player/Collisions.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FG
+{
+    public class Collisions : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.name == "Lava")
+            {
+                Debug.Log("You ded");
+            }
+            else if (collision.CompareTag("Spike"))
+            {
+                Debug.Log("Ouch");
+            }
+        }
+    }
+}
