ce8c151ad9f6f58a04b2741b5d8fa97c7cc4b31b
GitIn-space <martin@cjo.se>
2021-10-06 11:40:27 +0200

    Added ability to fall through platforms, use the S-key

--- Assets/Scripts/ScriptableObjects/Platform.cs
+++ Assets/Scripts/ScriptableObjects/Platform.cs
@@ -8,16 +8,34 @@ namespace FG
     {
         [SerializeField] private Transform player;
         [SerializeField] private Collider2D collider;
+        [SerializeField] private float passwindow = 0.5f;
+
+        [HideInInspector] private Coroutine passroutine;
+        [HideInInspector] private bool passing;
 
         private void Update()
         {
-            if (collider.enabled && player.position.y + 0.5 < transform.position.y)
-                collider.enabled = false;
+            if (!passing)
+            {
+                if (collider.enabled && player.position.y + 0.5 < transform.position.y)
+                    collider.enabled = false;
 
-            else if (!collider.enabled && player.position.y - 0.5 > transform.position.y)
-                collider.enabled = true;
+                else if (!collider.enabled && player.position.y - 0.5 > transform.position.y)
+                    collider.enabled = true;
+            }
+        }
 
+        private IEnumerator Timer()
+        {
+            yield return new WaitForSeconds(passwindow);
+            passing = false;
+        }
 
+        public void Fall()
+        {
+            passing = true;
+            collider.enabled = false;
+            passroutine = StartCoroutine("Timer");
         }
     }
 }
