23a3f01cca6a8723b5cf80a57226dd83c28ab77a
Sam Nilsson <sam64114208@gmail.com>
2021-10-06 10:27:20 +0200

    inventory UI and drop input

--- Assets/Scripts/Player/PlayerMovmentController.cs
+++ Assets/Scripts/Player/PlayerMovmentController.cs
@@ -6,41 +6,34 @@ public class PlayerMovmentController : MonoBehaviour
 {
     [NonSerialized] public bool Walking;
     [NonSerialized] public bool Jumping;
-    
+
     [SerializeField, Tooltip("Meters / second"), Header("Walking")]
     private float walkSpeed = 5f;
+
     [SerializeField, Tooltip("Seconds"), Header("Jumping")]
     private float maxJumpTime = .5f;
+
     [SerializeField, Tooltip("Meters / second")]
     private float jumpSpeed = 7f;
+
     [SerializeField] private LayerMask floorMaks;
 
     private Rigidbody2D body;
     private CapsuleCollider2D playerCollider;
     private int walkDirection = 0;
     private float currJumpTime = 0;
-    
+
     private const float SMALL_OFFSET = .1f;
 
     private void OnMove(InputValue value)
     {
         walkDirection = (int) value.Get<float>();
-        if (walkDirection != 0)
+        Walking = walkDirection != 0;
+        if (Walking)
             transform.localScale = new Vector3(walkDirection * -1, 1, 1);
     }
 
-    private void OnJump(InputValue value)
-    {
-        Jumping = value.isPressed && OnGround();
-        
-        /*if (value.isPressed && OnGround())
-        {
-            Jumping = true;
-            return;
-        }
-
-        Jumping = false;*/
-    }
+    private void OnJump(InputValue value) => Jumping = value.isPressed && OnGround();
 
 
     private bool OnGround() => Physics2D.BoxCast((Vector2) transform.position + playerCollider.offset,
@@ -52,9 +45,9 @@ public class PlayerMovmentController : MonoBehaviour
         Walk();
         Jump();
     }
-    
+
     private void Walk() => body.velocity = new Vector2((Vector2.right * walkDirection * walkSpeed).x, body.velocity.y);
-    
+
     private void Jump()
     {
         body.gravityScale = 1;
