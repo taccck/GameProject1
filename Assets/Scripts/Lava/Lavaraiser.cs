4e750d3435f5352c08275a59d927eb236c411c96
GitIn-space <martin@cjo.se>
2021-10-06 16:41:51 +0200

    Updating lava and spikes

--- Assets/Scripts/Lava/Lavaraiser.cs
+++ Assets/Scripts/Lava/Lavaraiser.cs
@@ -8,20 +8,47 @@ namespace FG
     {
         [SerializeField] private float speed;
         [SerializeField] private float heightcap;
+        [SerializeField] private int maxrange = 10;
+        [SerializeField] private float speedfactor = 1f;
+        [SerializeField] private bool multicatchup = false;
+        [SerializeField] private float speedupdateinterval = 1f;
+        [SerializeField] private Transform player;
 
         [HideInInspector] private Vector3 target;
+        [HideInInspector] private Coroutine updateroutine;
+        [HideInInspector] private float speedamp;
+
+        private IEnumerator Speedupdater()
+        {
+            while(true)
+            {
+                yield return new WaitForSeconds(speedupdateinterval);
+
+                if (player.position.y - transform.position.y > maxrange)
+                    if(multicatchup)
+                        speedamp = ((player.position.y - transform.position.y) / maxrange) * speedfactor;
+                    else
+                        speedamp = speedfactor;
+                else if(player.position.y - transform.position.y < maxrange)
+                    speedamp = 1f;
+            }
+        }
 
         private void FixedUpdate()
         {
             if(transform.position.y < heightcap)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-            }
+                transform.position = Vector3.MoveTowards(transform.position, target, speedamp * speed * Time.deltaTime);
         }
 
         private void Awake()
         {
             target = new Vector3(0, heightcap, 0);
+            updateroutine = StartCoroutine("Speedupdater");
+        }
+
+        private void OnDisable()
+        {
+            StopCoroutine(updateroutine);
         }
     }
 }
