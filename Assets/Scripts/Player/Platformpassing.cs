14f10f030b6509e4f8d576460f6b0d26889f4e7e
Sam Nilsson <sam64114208@gmail.com>
2021-10-07 12:02:57 +0200

    more conflicts grr

---
+++ Assets/Scripts/Player/Platformpassing.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+namespace FG
+{
+    public class Platformpassing : MonoBehaviour
+    {
+        [SerializeField] private Collider2D collider;
+        [SerializeField] private float passwindow = 0.5f;
+
+        [HideInInspector] private Coroutine passroutine;
+
+        private IEnumerator Timer()
+        {
+            yield return new WaitForSeconds(passwindow);
+            collider.enabled = true;
+        }
+
+        public void Fall()
+        {
+            collider.enabled = false;
+            passroutine = StartCoroutine("Timer");
+        }
+    }
+}
