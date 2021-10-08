1eddd3e1bdeb219c428e8205e253b01aa80af01d
GitIn-space <martin@cjo.se>
2021-10-08 12:51:02 +0200

    Added patroling enemies, collision for said enemies

--- Assets/Scripts/Enemies/Basicenemy.cs
+++ Assets/Scripts/Enemies/Basicenemy.cs
@@ -9,22 +9,36 @@ namespace FG
         [SerializeField] private int patrolwidth = 0;
         [SerializeField] private float speed = 0f;
 
-        [HideInInspector] private bool direction = false;
-        [HideInInspector] private Vector3 target;
+        [HideInInspector] private int direction = -1;
+        [HideInInspector] private float target;
+        [HideInInspector] private Rigidbody2D rigidbody;
 
         private void FixedUpdate()
         {
-            if(!direction)
-                if (transform.position.x >= target.x - patrolwidth)
-                    transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.x - patrolwidth, 0, 0), speed * Time.deltaTime);
+            if (direction < 0)
+                if (transform.position.x <= target)
+                {
+                    direction = 1;
+                    target = transform.position.x + patrolwidth;
+                }
+                else
+                    ;
             else
-                if (transform.position.x <= target.x)
-                    transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+                if (transform.position.x >= target)
+            {
+                direction = -1;
+                target = transform.position.x - patrolwidth;
+            }
+
+
+            rigidbody.velocity = new Vector2((Vector2.right * direction * speed).x, 0);
+            transform.localScale = new Vector3(direction * -1, 1, 1);
         }
 
         private void Awake()
         {
-            target = transform.position;
+            target = transform.position.x - patrolwidth;
+            rigidbody = GetComponent<Rigidbody2D>();
         }
     }
 }
--- Assets/Scripts/Player/Collisions.cs
+++ Assets/Scripts/Player/Collisions.cs
@@ -20,17 +20,32 @@ namespace FG
             invul = false;
         }
 
+        private void Collision()
+        {
+            if (!invul)
+            {
+                invul = true;
+                invulroutine = StartCoroutine("Invulperiod");
+                inventory.Drop();
+            }
+        }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.name == "Lava")
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             else if (collision.CompareTag("Danger"))
-                if (!invul)
-                {
-                    invul = true;
-                    invulroutine = StartCoroutine("Invulperiod");
-                    inventory.Drop();
-                }
+            {
+                Collision();
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.CompareTag("Danger"))
+            {
+                Collision();
+            }
         }
     }
 }
