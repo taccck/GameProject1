80f8bc9acbb5420a854e87490a70fb5e3924b265
Sam Nilsson <sam64114208@gmail.com>
2021-10-11 15:17:55 +0200

    added sound effects

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Curr;
+
+    public Sound[] sounds;
+    
+    [Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+    
+        [Range(0f,1f)] public float volume;
+        [Range(.1f,3f)] public float pitch;
+        public bool loop;
+
+        [HideInInspector] public AudioSource source;
+    }
+
+
+    private void Awake()
+    {
+        if (Curr == null)
+            Curr = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string inputName)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == inputName);
+        if (s == null)
+        {
+            Debug.LogError($"You made a typo stupid : {inputName}");
+            return;
+        }
+        s.source.Play();
+        
+    }
+}
