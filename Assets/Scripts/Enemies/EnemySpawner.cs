b42b7b2dceb4afb3941c261cc816afc1c0f079d3
Sam Nilsson <sam64114208@gmail.com>
2021-10-12 16:36:53 +0200

    range for spawner

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -1,26 +1,36 @@
 using System.Collections;
 using FG;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private GameObject enemyToSpawn;
     [SerializeField] private float spawnDelay = 5f;
+    [SerializeField] private float spawnDistance = 1f;
 
     private IEnumerator SpawnEnemy()
     {
         for (int i = 0; i < 1000; i++)
         {
+            float randomDistance = Random.Range(-spawnDistance, spawnDistance);
             yield return new WaitForSeconds(spawnDelay);
             GameObject newEnemy = Instantiate(enemyToSpawn);
             newEnemy.GetComponent<Fallingenemy>().player = player;
-            newEnemy.transform.position = transform.position;
+            newEnemy.transform.position = new Vector2(transform.position.x + randomDistance, transform.position.y);
         }
     }
 
     private void Start()
     {
         StartCoroutine(SpawnEnemy());
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + spawnDistance, transform.position.y));
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x - spawnDistance, transform.position.y));
     }
 }
