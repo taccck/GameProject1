b4d8f4029874afed724bbc60ed5627072f9d3290
Sam Nilsson <sam64114208@gmail.com>
2021-10-13 13:28:09 +0200

    added input to the cooking game

--- Assets/Scripts/Cooking/CookingController.cs
+++ Assets/Scripts/Cooking/CookingController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using FG;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CookingController : MonoBehaviour
 {
@@ -10,18 +12,91 @@ public class CookingController : MonoBehaviour
     [SerializeField] private Stayinzone progZone;
     [SerializeField] private Clickzone progClick;
 
-
     [Header("Animation"), SerializeField] private Sprite success;
     [SerializeField] private Sprite fail;
     [SerializeField] private SpriteRenderer toSwap;
     [SerializeField] private SpriteRenderer toHide;
     [SerializeField] private Animator toStop;
 
+    private enum EventType
+    {
+        Bar,
+        Circle,
+        Zone,
+        Click
+    }
+
+    private EventType eventType;
+    private Vector2 correctInput;
+    private CookingInputDisplay display;
+
+    private void OnQTE(InputValue value)
+    {
+        Vector2 buttonsPressed = value.Get<Vector2>();
+        if (buttonsPressed == Vector2.zero) return;
+
+        if (CheckInput(buttonsPressed))
+        {
+            AddScore();
+        }
+    }
+
+    private bool CheckInput(Vector2 input)
+    {
+        bool correctX = Mathf.Approximately(correctInput.x, input.x);
+        bool correctY = Mathf.Approximately(correctInput.y, input.y);
+        bool correct = correctX && correctY;
+        NewCorrectInput();
+
+        return correct;
+    }
+
+    private void NewCorrectInput()
+    {
+        switch (eventType)
+        {
+            case EventType.Bar:
+                correctInput = progBar.Checkinput();
+                break;
+            case EventType.Circle:
+                correctInput = progCircle.Checkinput();
+                break;
+            case EventType.Zone:
+                correctInput = progZone.Checkinput();
+                break;
+            case EventType.Click:
+                correctInput = progClick.Checkinput();
+                break;
+        }
+
+        display.SetUI(correctInput);
+    }
+
+    private void AddScore()
+    {
+        switch (eventType)
+        {
+            case EventType.Bar:
+                progBar.Interact();
+                break;
+            case EventType.Circle:
+                progCircle.Interact();
+                break;
+            case EventType.Zone:
+                progZone.Interact();
+                break;
+            case EventType.Click:
+                progClick.Interact(Vector2.zero);
+                break;
+        }
+    }
+
     public void Outcome(bool outcome)
     {
         if (toHide != null)
             toHide.color = new Color(0, 0, 0, 0);
-        toStop.enabled = false;
+        if (toStop != null)
+            toStop.enabled = false;
 
         if (fail == null || success == null) return;
 
@@ -31,26 +106,53 @@ public class CookingController : MonoBehaviour
     private IEnumerator CookTime()
     {
         yield return new WaitForSeconds(time);
-        if (progBar != null)
+
+        switch (eventType)
         {
-            Outcome(progBar.Isfilled());
-        }
-        else if (progCircle != null)
-        {
-            Outcome(progCircle.Isfilled());
-        }
-        else if (progZone != null)
-        {
-            Outcome(progZone.Isfilled());
-        }
-        else if (progClick != null)
-        {
-            Outcome(progClick.Isfilled());
+            case EventType.Bar:
+                Outcome(progBar.Isfilled());
+                break;
+            case EventType.Circle:
+                Outcome(progCircle.Isfilled());
+                break;
+            case EventType.Zone:
+                Outcome(progZone.Isfilled());
+                break;
+            case EventType.Click:
+                Outcome(progClick.Isfilled());
+                break;
         }
     }
 
     private void Start()
     {
         StartCoroutine(CookTime());
+        if (progBar != null)
+        {
+            eventType = EventType.Bar;
+            progBar.Startbar();
+        }
+        else if (progCircle != null)
+        {
+            eventType = EventType.Circle;
+            progCircle.Startbar();
+        }
+        else if (progZone != null)
+        {
+            eventType = EventType.Zone;
+            progZone.Startbar();
+        }
+        else if (progClick != null)
+        {
+            eventType = EventType.Click;
+            progClick.Startbar();
+        }
+
+        NewCorrectInput();
+    }
+
+    private void Awake()
+    {
+        display = GetComponentInChildren<CookingInputDisplay>();
     }
 }
---
+++ Assets/Scripts/Cooking/CookingInputDisplay.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CookingInputDisplay : MonoBehaviour
+{
+   [SerializeField] private Sprite[] wasdSprites;
+
+   private SpriteRenderer spriteRenderer;
+
+   public void SetUI(Vector2 button)
+   {
+      if (Mathf.Approximately(button.y, 1f))
+         spriteRenderer.sprite = wasdSprites[0];
+      else if(Mathf.Approximately(button.x, 1f))
+         spriteRenderer.sprite = wasdSprites[1];
+      else if(Mathf.Approximately(button.y, -1f))
+         spriteRenderer.sprite = wasdSprites[2];
+      else if(Mathf.Approximately(button.x, -1f))
+         spriteRenderer.sprite = wasdSprites[3];
+   }
+
+   private void Awake()
+   {
+      spriteRenderer = GetComponent<SpriteRenderer>();
+   }
+}
