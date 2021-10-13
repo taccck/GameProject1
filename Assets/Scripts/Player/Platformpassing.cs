55e782dac6f98b2dbb765120546f5fec011b1a14
GitIn-space <martin@cjo.se>
2021-10-13 15:28:51 +0200

    Toggle to freeeze x-movement while falling through platforms

--- Assets/Scripts/Player/Platformpassing.cs
+++ Assets/Scripts/Player/Platformpassing.cs
@@ -6,16 +6,33 @@ namespace FG
     public class Platformpassing : MonoBehaviour
     {
         [SerializeField] private Collider2D collider;
+        [SerializeField] private bool freezex = false;
+
+        [HideInInspector] private Rigidbody2D body;
 
         public void Fall()
         {
             collider.isTrigger = true;
+
+            if(freezex)
+                body.constraints = RigidbodyConstraints2D.FreezePositionX;
+
         }
 
         private void OnTriggerExit2D(Collider2D collision)
         {
             if (collision.CompareTag("Platform"))
+            {
                 collider.isTrigger = false;
+
+                if(freezex)
+                    body.constraints = RigidbodyConstraints2D.FreezeRotation;
+            }
+        }
+
+        private void Awake()
+        {
+            body = GetComponent<Rigidbody2D>();
         }
     }
 }
