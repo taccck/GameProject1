43f50474dac7825e97e0c400d2be636e01ec3f19
GitIn-space <martin@cjo.se>
2021-10-14 13:54:14 +0200

    Added blinking warning to the spawner

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -9,14 +9,39 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject enemyToSpawn;
     [SerializeField] private float spawnDelay = 5f;
     [SerializeField] private float spawnDistance = 1f;
+    [SerializeField] private float blinkduration = 0.5f;
+    [SerializeField] private float blinkdelay = 3f;
+    [SerializeField] private GameObject arrowprefab;
 
     [HideInInspector] private bool started = false;
+
+    private IEnumerator Blink(SpriteRenderer sr)
+    {
+        while (true)
+        {
+            sr.enabled = !sr.enabled;
+            yield return new WaitForSeconds(blinkduration);
+        }
+    }
+
+    private IEnumerator Warning(float x)
+    {
+        yield return new WaitForSeconds(blinkdelay);
+        GameObject go = Instantiate(arrowprefab, Camera.main.transform);
+        go.transform.localPosition += new Vector3(x, 0, -Camera.main.transform.position.z);
+        StartCoroutine(Blink(go.GetComponent<SpriteRenderer>()));
+        yield return new WaitForSeconds(spawnDelay - blinkdelay);
+        Destroy(go);
+    }
 
     private IEnumerator SpawnEnemy()
     {
         for (int i = 0; i < 1000; i++)
         {
             float randomDistance = Random.Range(-spawnDistance, spawnDistance);
+
+            StartCoroutine(Warning(randomDistance));
+
             yield return new WaitForSeconds(spawnDelay);
             GameObject newEnemy = Instantiate(enemyToSpawn);
             if (player != null)
