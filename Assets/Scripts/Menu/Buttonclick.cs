91dc9343e7592e476504981ad3f8a39408babd69
GitIn-space <martin@cjo.se>
2021-10-14 10:51:57 +0200

    Menu scene, buttons, font, background, button-fucntion todo

---
+++ Assets/Scripts/Menu/Buttonclick.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FG
+{
+    public class Buttonclick : MonoBehaviour
+    {
+        public void Playclick()
+        {
+            Debug.Log("herp");
+        }
+
+        public void Quitclick()
+        {
+            Debug.Log("derp");
+        }
+    }
+}
